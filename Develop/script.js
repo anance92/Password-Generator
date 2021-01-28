7a715d3af8ec3083a67914e336652e357d748c6e
unknown <alexandrannance@gmail.com>
2021-01-28 07:54:56 -0600

    Add the rest of the character types and randomized password generated

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,6 @@ let specialCharacters = "!$%&'()*+,-./:;<=>?@[]^_`{|}~"
 let password = "";
 
 // create the generatePassword function
-
 const generatePassword = () => {
 
   // need to get the password length
@@ -25,9 +24,7 @@ const generatePassword = () => {
     }
     return newPasswordLength;
   }
-  // call the password length function and assign the input to newPasswordLength
   newPasswordLength = getPasswordLength();
-
   // need to have special characters or not
   let getCharTypes = () => {
     // lowercase
@@ -38,6 +35,51 @@ const generatePassword = () => {
       window.alert("Please choose yes or no!");
       return getCharTypes();
     }
+
+    //uppercase
+    let charUpperCase = window.prompt("Would you like your password to contain uppercase characters? Please type yes or no.").toLowerCase();
+    if (charUpperCase === "yes") { charUpperCase = alphabetCap }
+    else if (charUpperCase === "no") { charUpperCase = null }
+    else {
+      window.alert("Please choose yes or no!");
+      return getCharTypes();
+    }
+
+    //numbers
+    let charNumbers = window.prompt("Would you like your password to contain numbers? Please type yes or no.").toLowerCase();
+    if (charNumbers === 'yes') { charNumbers = numbers }
+    else if (charNumbers === 'no') { charNumbers = null }
+    else {
+      window.alert("Please choose yes or no!");
+      return getCharTypes();
+    }
+
+    //special characters
+    let charSpecial = window.prompt("Would you like your password to contain special characters? Please type yes or no.").toLowerCase();
+    if (charSpecial === "yes") { charSpecial = specialChars }
+    else if (charSpecial === "no") { charSpecial = null }
+    else {
+      window.alert("Please choose yes or no!");
+      return getCharTypes();
+    }
+
+    //takes array of all character types and converts them to one string
+    let passwordChars = [charLowerCase, charUpperCase, charSpecial, charNumbers].join("")
+    //return to beginning of function if no valid selection are made.
+    if (!passwordChars) {
+      window.alert("You must select at least one type of character for your password! Please try again!");
+      return getCharTypes();
+    } else {
+      window.alert("Conditions accepted! Preparing to generate password now!")
+
+    }
+    return passwordChars;
+  }
+  passwordChars = getCharTypes();
+  password = "";
+  for (i = 0; i < newPasswordLength.length; i++) {
+      let c = Math.floor(Math.random() * passwordChars.length);
+      password += passwordChars.charAt(c)
   }
   return password;
 }
@@ -49,9 +91,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
